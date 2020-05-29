879331f93b9774f616f9da1c7a5ce90e296eed97
RockLikeBusStop <arasam1067@gmail.com>
2020-05-29 13:41:49 -0400

    my_stack.rs is added It contains the stack stuct that is used in solving the n_queens problem
    Co-authored-by: Awildo <agutierr@hamilton.edu>
    Co-authored-by: Siqi Fang <2581703212@qq.com>
    Co-authored-by: Sosina-abuhay <sabuhay@hamilton.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,26 @@
 /*
 * Authors: Awildo G., Sam. A., Siqi F., Sosina A.
-* Date: May 28, 2020
+* Date: May 29, 2020
 * Project: N Queens Problem
 * Desc: Is it possible to fit N Queen Pieces in an N x N board such that no Queen can be taken?
 */
 
 use std::fmt;
-use std::marker::Copy; // we want to be able to return copies of objects
 
-// We implement Stack for displayable and copy-able items
-pub struct Stack<T: fmt::Display + Copy> {
-    // The top of the stack is the last item in contents
-    size: usize,
-    contents: Vec<T>,
-}
+// uses stack.rs in the src "mod" = module
+mod my_stack;
 
-impl<T: fmt::Display + Copy> Stack<T> {
-    // Constructor of Objects of type Stack.
-    pub fn new() -> Self {
-        Self {
-            size: 0,
-            contents: Vec::new(),
-        }
-    }
-
-    // Returns the top element of the Stack
-    pub fn top(&self) -> T {
-        assert!(!self.empty());
-        self.contents[self.size - 1]
-    }
-
-    // Pushes an item onto the top of the Stack
-    pub fn push(&mut self, item: T) {
-        self.contents.push(item);
-        self.size += 1;
-    }
-
-    // Pop removes the top of the Stack
-    pub fn pop(&mut self) -> T {
-        assert!(!self.empty());
-        self.size -= 1;
-
-        // deletes the top and returns the top
-        self.contents.pop().unwrap()
-    }
-
-    // Returns the size of the Stack
-    pub fn size(&self) -> usize {
-        self.size
-    }
-
-    // Returns true when empty, false otherwise
-    pub fn empty(&self) -> bool {
-        self.size == 0
-    }
-
-    // Clears the stack and its contents
-    pub fn clear(&mut self) {
-        self.contents.clear();
-        self.size = 0;
-    }
-    // Prints the Stack from the bottom to top.
-    pub fn print(&self) {
-        if self.empty() {
-            println!("[]");
-            return;
-        }
-
-        println!("From bottom to top [Bottom ... Top], the Stack contains: ");
-        print!("[");
-        for (idx, item) in self.contents.iter().enumerate() {
-            if idx < self.size - 1 {
-                print!("{}, ", item);
-            } else {
-                println!("{}]", item);
-            }
-        }
-    }
-}
+// crate is the program (either binary or library, we have binary)
+// pub use means 
+pub use crate::my_stack::Stack;
 
 // the number of queens on the board, and the size of the board
-const N: usize = 30;
+const N: usize = 8;
 
 pub struct Board {
     // board is an array of booleans
-    // TRUE => YAS QUEEN
-    // FALSE => NAH QUEEN
+    // true => there is a queen 
+    // false => empty space
     board: [[bool; N]; N],
 }
 
